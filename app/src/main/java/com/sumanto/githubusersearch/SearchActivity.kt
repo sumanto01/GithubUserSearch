f2c259dfa4b126a2d066078bda8da2f41b889698
sumanto <sumanto1x@gmail.com>
2020-08-15 21:12:22 +0700

    update colors

--- app/src/main/java/com/sumanto/githubusersearch/SearchActivity.kt
+++ app/src/main/java/com/sumanto/githubusersearch/SearchActivity.kt
@@ -3,6 +3,9 @@ package com.sumanto.githubusersearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Created by sumanto on 8/15/20.
+ */
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
